530ffefe2c2b1452a5f235fcf9cba107b3ede58e
Artjoms Kasperovics <90187055+ar4cha@users.noreply.github.com>
2023-01-25 08:12:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def fibionacci(n):
+    old, new = 0, 1
+    for _ in range(n):
+        old, new = new, old + new
+    return old
+
+
+# Rekursīva Fibonacci skaitļa aprēķināšanas funkcija
+def rfibionacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return rfibionacci(n-1) + rfibionacci(n-2)
+       
+
+
+fibionacci(5)
+rfibionacci(3)
